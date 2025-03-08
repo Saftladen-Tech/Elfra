215a017035edd0ebc9d2223ae475e259810c5707
Flo <93609174+Xeferis@users.noreply.github.com>
2025-03-08 09:50:59 +0100

    Added Base Content

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
+<script setup>
+const { data: home_content } = await useAsyncData('home_content', () => queryCollection('content').path("/home").first());
+</script> 
+
 <template>
   <div>
     Home
     <placeholder />
+    <ContentRenderer :value="home_content" />
   </div>
 </template>
