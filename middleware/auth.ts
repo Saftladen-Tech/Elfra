c8386505ab4972024c2c93f7837e1d1acd178b9c
Florian <93609174+Xeferis@users.noreply.github.com>
2025-03-10 09:50:00 +0000

    Added hint

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,6 +2,9 @@ export default defineNuxtRouteMiddleware((to, from) => {
     let auth: boolean = true
 
     if (!auth) {
+        console.log("Not Authenticated")
         return navigateTo('/login')
+    } else {
+        console.log("Authentication successful")
     }
   })
