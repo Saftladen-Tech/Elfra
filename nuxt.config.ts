96a5d215fe05ceec815279b3e8b914f62fd5faf8
Flo <93609174+Xeferis@users.noreply.github.com>
2025-03-14 20:18:36 +0100

    Removed Sidebase auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/content", "@nuxt/ui", "@sidebase/nuxt-auth"],
+  modules: ["@nuxt/content", "@nuxt/ui"],
   colorMode: {
     preference: "system",
   },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-
-  auth: {
-    isEnabled: true,
-    //disableServerSideAuth: false,
-    provider: {
-      type: 'authjs',
-      trustHost: false,
-      defaultProvider: 'github',
-      addDefaultCallbackUrl: true
-    },
-    sessionRefresh: {
-      enablePeriodically: true,
-      enableOnWindowFocus: true,
-    }
-  }
-
 });
