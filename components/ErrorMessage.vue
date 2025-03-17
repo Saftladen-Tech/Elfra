7df19800158da7b531442435256a4fe521a5dacf
Florian <93609174+Xeferis@users.noreply.github.com>
2025-03-17 10:20:39 +0000

    Added component

---
+++ components/ErrorMessage.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="flex w-full justify-center items-center">
+        <h1>404</h1>
+    </div>
+</template>
--- pages/error.vue
+++ pages/error.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <placeholder />
+    <ErrorMessage />
   </div>
 </template>
