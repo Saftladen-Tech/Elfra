11a9452787ca89ca59e6077eba2460f6381a6dc5
Flo <93609174+Xeferis@users.noreply.github.com>
2025-03-20 18:36:12 +0100

    Added basic content handling - could change

--- pages/chapter_content.vue
+++ pages/chapter_content.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-const md_topic_content = await queryCollection('content').path('/content_placeholder').first()
+const content = await queryCollection('content').path('/content_placeholder').first()
 </script>
 
 <template>
   <div class="flex flex-col min-h-screen xl:px-28 my-20">
-    <h1 id="md-course" class="text-5xl font-bold text-prmry-500">Chapter xy</h1>
+    <h1 id="md-course" class="text-5xl font-bold text-prmry-500">{{ content.title }}</h1>
     <div id="content" class="bg-white p-5 rounded-lg min-h-full my-4 text-black">
-      <h2 id="md-topic" class="text-4xl font-semibold mb-5">Topic xy</h2>
-      <ContentRenderer class="text-xl font-regular" :value="md_topic_content" />
+      <h2 id="md-topic" class="text-4xl font-semibold mb-5">{{ content.description }}</h2>
+      <ContentRenderer class="text-xl font-regular" :value="content.body" />
     </div>
     <div id="buttons" class="flex justify-between my-3">
       <UButton variant="outline" class="px-14 py-4">Zurück</UButton>
