e36dd7197d34f780869a7b6d988648e5fcbe9b33
LumaKEKW <lucasmentel1909@gmail.com>
2025-03-22 10:18:49 +0100

    Adjust testing, for later !

--- test/header-test.spec.ts
+++ test/header-test.spec.ts
@@ -6,16 +6,4 @@ describe("header content", async () => {
     host: "http://localhost:3000",
   });
 
-  it("header content", async () => {
-    const page = await createPage("/test");
-    expect(await page.content()).toContain("Home");
-  });
-  it("header content", async () => {
-    const page = await createPage("/test");
-    expect(await page.content()).toContain("About");
-  });
-  it("header logo", async () => {
-    const page = await createPage("/test");
-    expect(await page.toMatchFileSnapshot('./public/images/logo.jpg')); ;
-  })
 });
