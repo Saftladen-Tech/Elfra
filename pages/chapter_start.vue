22e0e7eff0ba6300717ad72a4e39193d7ecb672e
Flo <93609174+Xeferis@users.noreply.github.com>
2025-03-27 17:45:21 +0100

    Added fix for footer

--- pages/chapter_start.vue
+++ pages/chapter_start.vue
@@ -2,7 +2,7 @@
 </script> 
 
 <template>
-  <div class="flex justify-center items-center">
+  <div class="flex justify-center items-center grow">
       <div class="flex flex-col justify-center items-center max-w-[800px] space-y-8">
         <h1 class="text-8xl font-semibold darK:drop-shadow-glow-lg drop-shadow-glow-dark-lg">Topic xy</h1>
         <p class="text-4xl text-center">Hier kommt eine kuze beschreibung der Topic hin. Kann alles mögliche sein!</p>
