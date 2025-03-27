840b552612ee62d9995998debc7b47408d4e15b5
Flo <93609174+Xeferis@users.noreply.github.com>
2025-03-27 18:39:20 +0100

    Added full layout and design

--- pages/chapter_end.vue
+++ pages/chapter_end.vue
@@ -1,5 +1,23 @@
+<script setup lang="ts">
+import { UIcon } from '#components';
+
+</script>
+
 <template>
-  <div>
-    <placeholder />
+  <div class="flex flex-col grow justify-center items-center">
+    <div class="flex flex-col justify-center items-center text-center space-y-12">
+      <div>
+        <h1 class="text-8xl text-semibold">Kapitel abgeschlossen!</h1>
+      </div>
+      <div class="dark:text-success-500 text-success-700">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-64">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+        </svg>
+      </div>
+      <div class="flex space-x-24">
+        <UButton variant="soft" icon="i-heroicons-arrow-left" class="flex justify-center items-center  p-4 w-56 text-center text-2xl">Wiederholen</UButton>
+        <UButton variant="soft" trailing-icon="i-heroicons-arrow-right" class="flex justify-center items-center p-4 w-56 text-center text-2xl">Weiter</UButton>
+      </div>
+    </div>
   </div>
 </template>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ export default <Partial<Config>>{
         accnt: generateTailwindShades("#fde1af"),
         drk: generateTailwindShades("#0e0e11"),
         brght: generateTailwindShades("#ffffff"),
+        success: generateTailwindShades("#00DC82"),
       },
       dropShadow: {
         "glow-bright-lg": "0 0 50px theme('colors.prmry.300')",
