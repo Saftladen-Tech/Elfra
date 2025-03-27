e2673519cfd46821d6b855bf388b18668244ded3
Flo <93609174+Xeferis@users.noreply.github.com>
2025-03-27 18:05:08 +0100

    Added grow

--- pages/chapter_content.vue
+++ pages/chapter_content.vue
@@ -3,7 +3,7 @@ const content = await queryCollection('content').path('/content_placeholder').fi
 </script>
 
 <template>
-  <div class="flex flex-col min-h-screen xl:px-28 my-20">
+  <div class="flex flex-col min-h-screen xl:px-28 my-20 grow">
     <h1 id="md-course" class="text-5xl font-bold text-prmry-500">{{ content.title }}</h1>
     <div id="content" class="bg-white p-5 rounded-lg min-h-full my-4 text-black border light:border-black dark:border-prmry-500/50">
       <h2 id="md-topic" class="text-4xl font-semibold mb-5">{{ content.description }}</h2>
