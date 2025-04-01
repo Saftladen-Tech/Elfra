415f8cfde7eb13a95a5a66c1999a9a36183f2bd6
LumaKEKW <lucasmentel1909@gmail.com>
2025-04-01 16:29:46 +0200

    Adjust Index Page based on prototype.

--- pages/index.vue
+++ pages/index.vue
@@ -23,11 +23,11 @@ const rows = [
 </script> 
 
 <template>
-  <div class="flex flex-col w-full max-w-4xl mx-auto mt-8 grow">
+  <div class="flex flex-col w-full max-w-5xl mx-auto mt-8 grow">
     <h1 class="text-7xl mb-4 text-prmry-500">Welcome to Elfra</h1>
-    <p class="text-5xl mb-4">the Framework for e-learning and WBTs</p>
+    <p class="text-5xl mb-12">the Framework for e-learning and WBTs</p>
     <UDivider icon="i-heroicons-book-open" size="sm" :ui="{border:{base:'dark:border-gray-400'}, container:{base:'dark:text-white'}}"></UDivider>
-    <h2 class="text-3xl mt-4 mb-4">Webbasedtrainings and Content</h2>
+    <h2 class="text-3xl mt-6 mb-4">Webbasedtrainings and Content</h2>
 
     <UTable :rows="rows" :columns="columns" class="rounded-lg border border-gray-400" :ui="{td: {color:'dark:text-gray-50'}, divide:'dark:divide-gray-300', tbody:'dark:divide-gray-400'}">
       <template #topic-data="{ row }">
