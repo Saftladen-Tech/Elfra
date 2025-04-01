92d85628cdae58280e638929a89baf23d35ce9f1
LumaKEKW <lucasmentel1909@gmail.com>
2025-04-01 17:33:28 +0200

    readjust tailwindconfig

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,15 +11,9 @@ export default <Partial<Config>>{
         accnt: generateTailwindShades("#fde1af"),
         drk: generateTailwindShades("#0e0e11"),
         brght: generateTailwindShades("#ffffff"),
-<<<<<<< HEAD
-        muted: generateTailwindShades("#a1a1a1"),
-        err: generateTailwindShades("#ff6467"),
-        brdr: generateTailwindShades("#262626"),
-=======
         success: generateTailwindShades("#00DC82"),
         warn: generateTailwindShades("#fcc800"),
         err: generateTailwindShades("#ff6467"),
->>>>>>> main
       },
       dropShadow: {
         "glow-bright-lg": "0 0 50px theme('colors.prmry.300')",
