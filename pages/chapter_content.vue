7b50c26b7daf65a41e812b313dcf524b86612865
Flo <93609174+Xeferis@users.noreply.github.com>
2025-04-03 18:28:29 +0200

    Working tests

--- pages/chapter_content.vue
+++ pages/chapter_content.vue
@@ -5,7 +5,7 @@ const content = await queryCollection('content').path('/content_placeholder').fi
 <template>
   <div class="flex flex-col min-h-screen xl:px-28 my-20 grow">
     <h1 id="md-course" class="text-5xl font-bold text-prmry-500">{{ content.title }}</h1>
-    <div id="content" class="bg-white p-5 rounded-lg min-h-full my-4 text-black border light:border-black dark:border-prmry-500/50">
+    <div data-testid="content" class="bg-white p-5 rounded-lg min-h-full my-4 text-black border light:border-black dark:border-prmry-500/50">
       <h2 id="md-topic" class="text-4xl font-semibold mb-5">{{ content.description }}</h2>
       <ContentRenderer class="text-xl font-regular" :value="content.body" />
     </div>
--- test/chapter-content-test.spec.ts
+++ test/chapter-content-test.spec.ts
@@ -7,6 +7,27 @@ describe("Chapter-Content test", async () => {
   });
 
   it("Has Chapter Heading", async () => {
-    //to be filled!
+    const page = await createPage("/chapter_content");
+    expect(await page.getByRole("heading", { level: 1 }).count()).toBe(1);
+  });
+
+  it("Has Topic Heading", async () => {
+    const page = await createPage("/chapter_content");
+    const content = page.getByTestId("content");
+    expect(await content.getByRole("heading", { level: 2 }).count()).toBe(1);
+  });
+
+  it("Has Nav Buttons next", async () => {
+    const page = await createPage("/chapter_content");
+    expect(
+      await page.getByRole("link", { name: "Weiter", exact: true }).count()
+    ).toBe(1);
+  });
+
+  it("Has Nav Buttons prev", async () => {
+    const page = await createPage("/chapter_content");
+    expect(
+      await page.getByRole("link", { name: "Zurück", exact: true }).count()
+    ).toBe(1);
   });
 });
