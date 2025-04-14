64a974bd4f0a15e9beb66e2ac6666e85a9fa499c
Florian <93609174+Xeferis@users.noreply.github.com>
2025-04-14 06:05:04 +0000

    test added logout

---
+++ test/logout-test.spec.ts
@@ -0,0 +1,13 @@
+import { describe, it, expect, test } from "vitest";
+import { setup, createPage, $fetch } from "@nuxt/test-utils/e2e";
+
+describe("logout test", async () => {
+  await setup({
+    host: "http://localhost:3000",
+  });
+
+  it("logout content", async () => {
+    const page = await createPage("/");
+    expect(await page.content()).toContain("");
+  });
+});
