a53e83e5fcae3431bdd6aad6e5c2991f922ada4a
Florian <93609174+Xeferis@users.noreply.github.com>
2025-04-14 06:00:56 +0000

    test added course-start

---
+++ test/course-start-test.spec.ts
@@ -0,0 +1,13 @@
+import { describe, it, expect, test } from "vitest";
+import { setup, createPage, $fetch } from "@nuxt/test-utils/e2e";
+
+describe("course start test", async () => {
+  await setup({
+    host: "http://localhost:3000",
+  });
+
+  it("course start content", async () => {
+    const page = await createPage("/");
+    expect(await page.content()).toContain("");
+  });
+});
