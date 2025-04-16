4ad43a4bb00655c9583fd2009aaa3fea3feee099
Florian <93609174+Xeferis@users.noreply.github.com>
2025-04-16 07:47:09 +0000

    Added tests

--- test/course-end-test.spec.ts
+++ test/course-end-test.spec.ts
@@ -6,8 +6,48 @@ describe("course end test", async () => {
     host: "http://localhost:3000",
   });
 
-  it("course end content", async () => {
+  it("static content", async () => {
     const page = await createPage("/course_end");
-    expect(await page.content()).toContain("");
+    const heading = await page.getByRole("heading",{level: 1});
+    expect(await heading.allInnerTexts()).toContain("Kurs beendet!");
+    expect(await page.content()).toContain("Herzlichen glückwunsch! Du kannst den Kurs nun erneut durcharbeiten oder Abschließen!");
+  });
+
+  it("Content position", async () => {
+    const page = await createPage("/course_end");
+
+    const content = await page.getByTestId("contentwrapper"); // Adjust selector if needed
+    const contentbox = await content.boundingBox();
+
+    const vpw = await page.evaluate(() => window.innerWidth);
+
+    const elementCenterX = contentbox.x + contentbox.width / 2;
+
+    const vpxc = vpw / 2;
+
+    expect(elementCenterX).toBeCloseTo(vpxc, 2); // Allow slight margin
+  });
+
+  it("Has ICON", async () => {
+    const page = await createPage("/course_end");
+    const content = page.getByTestId("icon");
+    expect(await content.count()).toBe(1);
+  });
+
+
+  it("Has Nav Buttons end", async () => {
+    const page = await createPage("/course_end");
+    const navigation = page.getByTestId("navigation");
+    expect(
+      await navigation.getByRole("button", { name: "Beenden", exact: true }).count()
+    ).toBe(1);
+  });
+
+  it("Has Nav Buttons repeat", async () => {
+    const page = await createPage("/course_end");
+    const navigation = page.getByTestId("navigation");
+    expect(
+      await navigation.getByRole("button", { name: "Neustarten", exact: true }).count()
+    ).toBe(1);
   });
 });
