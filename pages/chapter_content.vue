49f9cfd2cbb70c6be15892bcac2df25024a9786e
Flo <93609174+Xeferis@users.noreply.github.com>
2025-04-16 18:23:44 +0200

    Fixed layout not displaying

--- pages/chapter_content.vue
+++ pages/chapter_content.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
-const content = await queryCollection('content').path('/courses/placeholderCourse/1-Introduction/1-Start').first()
+const {data: content} = await useAsyncData('/courses/test-course/3-deepdive/1-theway', () => {
+  return queryCollection('content').path('/courses/test-course/3-deepdive/1-theway').first()
+})
+console.log(content)
 </script>
 
 <template>
@@ -7,7 +10,7 @@ const content = await queryCollection('content').path('/courses/placeholderCours
     <h1 id="md-course" class="text-5xl font-bold text-prmry-500">{{ content.title }}</h1>
     <div id="content" class="bg-white p-5 rounded-lg min-h-full my-4 text-black border light:border-black dark:border-prmry-500/50">
       <h2 id="md-topic" class="text-4xl font-semibold mb-5">{{ content.description }}</h2>
-      <ContentRenderer class="text-xl font-regular" :value="content.body" />
+      <ContentRenderer class="text-xl font-regular" :value="content" />
     </div>
     <div id="buttons" class="flex justify-between my-3">
       <UButton color="primary" variant="outline" class="px-14 py-4" to="/">Zurück</UButton>
