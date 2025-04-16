cc04ecc9c0e9497174ac40212920868886df2f25
Flo <93609174+Xeferis@users.noreply.github.com>
2025-04-16 17:05:25 +0200

    Fixed glow issue

--- pages/chapter_start.vue
+++ pages/chapter_start.vue
@@ -5,7 +5,7 @@
   <div class="flex justify-center items-center grow">
       <div data-testid="contentwrapper" class="flex flex-col justify-center items-center max-w-[800px] space-y-8">
         <h1 class="text-8xl font-semibold dark:drop-shadow-glow-lg drop-shadow-glow-dark-lg">Topic xy</h1>
-        <p class="text-4xl text-center">Hier kommt eine kurze beschreibung der Topic hin. Kann alles mögliche sein!</p>
+        <p class="text-4xl text-center bg-transparent">Hier kommt eine kurze beschreibung der Topic hin. Kann alles mögliche sein!</p>
         <UButton data-testid="startbtn" variant="ghost" size="xl" icon="i-heroicons-arrow-down"></UButton>
       </div>
   </div>
