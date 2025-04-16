efa0d70acd95e1425cf16a54951f91ab991434b5
Florian <93609174+Xeferis@users.noreply.github.com>
2025-04-16 07:07:33 +0000

    Tests added

--- test/chapter-end-test.spec.ts
+++ test/chapter-end-test.spec.ts
@@ -7,8 +7,31 @@ describe("chapter end test", async () => {
     //host: "http://localhost:3000",
   });
 
-  it("chapter end content", async () => {
+  it("Static Content", async () => {
     const page = await createPage("/chapter_end");
-    expect(await page.content()).toContain("");
+    expect(await page.content()).toContain("Kapitel abgeschlossen!");
+  });
+
+  it("Has ICON", async () => {
+    const page = await createPage("/chapter_end");
+    const content = page.getByTestId("icon");
+    expect(await content.count()).toBe(1);
+  });
+
+
+  it("Has Nav Buttons next", async () => {
+    const page = await createPage("/chapter_end");
+    const navigation = page.getByTestId("navigation");
+    expect(
+      await navigation.getByRole("link", { name: "Weiter", exact: true }).count()
+    ).toBe(1);
+  });
+
+  it("Has Nav Buttons repeat", async () => {
+    const page = await createPage("/chapter_end");
+    const navigation = page.getByTestId("navigation");
+    expect(
+      await navigation.getByRole("link", { name: "Wiederholen", exact: true }).count()
+    ).toBe(1);
   });
 });
