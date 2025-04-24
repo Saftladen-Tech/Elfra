f9cd65c2fc774b47daecaf8b6e11b5a52561dc51
LumaKEKW <lucasmentel1909@gmail.com>
2025-04-24 18:33:23 +0200

    Added theme.ts as configfile from external tool. Edited tailwindconfig

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,20 @@
 import type { Config } from "tailwindcss";
 import defaultTheme from "tailwindcss/defaultTheme";
 import generateTailwindShades from "./utils/js/colorGen.js";
+import { theme as colorConfig} from "./utils/js/theme.js";
 
 export default <Partial<Config>>{
   theme: {
     extend: {
       colors: {
-        prmry: generateTailwindShades("#669c35"),
-        scndry: generateTailwindShades("#aaaaaa"),
-        accnt: generateTailwindShades("#fde1af"),
-        drk: generateTailwindShades("#0e0e11"),
-        brght: generateTailwindShades("#ffffff"),
-        success: generateTailwindShades("#00DC82"),
-        warn: generateTailwindShades("#fcc800"),
-        err: generateTailwindShades("#ff6467"),
+        prmry: generateTailwindShades(colorConfig.colors.primary),
+        scndry: generateTailwindShades(colorConfig.colors.secondary),
+        accnt: generateTailwindShades(colorConfig.colors.accnt),
+        drk: generateTailwindShades(colorConfig.colors.dark),
+        brght: generateTailwindShades(colorConfig.colors.bright),
+        success: generateTailwindShades(colorConfig.colors.success),
+        warn: generateTailwindShades(colorConfig.colors.warn),
+        err: generateTailwindShades(colorConfig.colors.err),
       },
       dropShadow: {
         "glow-bright-lg": "0 0 50px theme('colors.prmry.300')",
---
+++ utils/js/theme.ts
@@ -0,0 +1,13 @@
+export const theme = {
+    colors: {
+      primary: '#669c35',
+      secondary: '#aaaaaa',
+      accnt: '#fde1af',
+      dark: '#0e0e11',
+      bright: '#ffffff',
+      success: '#00DC82',
+      warn: '#fcc800',
+      err: '#ff6467',
+    },
+  };
+  
