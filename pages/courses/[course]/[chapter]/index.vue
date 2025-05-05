4ed7d57a660cee3817aced45a150051be9d7635b
Florian <93609174+Xeferis@users.noreply.github.com>
2025-05-05 09:11:59 +0000

    Added routing to chapter start

--- pages/courses/[course]/[chapter]/index.vue
+++ pages/courses/[course]/[chapter]/index.vue
@@ -1,12 +1,24 @@
 <script setup lang="ts">
-</script> 
+const route = useRoute()
+const currentcourse = useCookie('selectedcourse')
+
+currentcourse.value = route.params.course
+
+const { data: start_page } = await useAsyncData("start_page", () => {
+  return queryCollection('chapters')
+    .where('path', 'LIKE', "%/"+ currentcourse.value +"/%")
+    .first()
+})
+
+</script>
+
 
 <template>
   <div class="flex justify-center items-center grow">
       <div data-testid="contentwrapper" class="flex flex-col justify-center items-center max-w-[800px] space-y-8">
-        <h1 class="text-8xl font-semibold dark:drop-shadow-glow-lg drop-shadow-glow-dark-lg">Topic xy</h1>
+        <h1 class="text-8xl font-semibold dark:drop-shadow-glow-lg drop-shadow-glow-dark-lg">{{ route.params.chapter }}</h1>
         <p class="text-4xl text-center bg-transparent">Hier kommt eine kurze beschreibung der Topic hin. Kann alles mögliche sein!</p>
-        <UButton data-testid="startbtn" variant="ghost" size="xl" icon="i-heroicons-arrow-down"></UButton>
+        <UButton data-testid="startbtn" variant="ghost" size="xl" icon="i-heroicons-arrow-down" :to="start_page?.path"></UButton>
       </div>
   </div>
 </template>
