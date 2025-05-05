eef8343570541647400d1de6f8a4a01267fa44d1
Florian <93609174+Xeferis@users.noreply.github.com>
2025-05-05 09:43:02 +0000

    Added topic start routing

--- pages/courses/[course]/index.vue
+++ pages/courses/[course]/index.vue
@@ -10,6 +10,9 @@ const { data: start_page } = await useAsyncData("start_page", () => {
     .first()
 })
 
+const start_chapter = start_page.value?.path
+console.log("Start chapter: ", start_chapter)
+const start_route = "/courses/" + currentcourse.value + "/" + start_chapter?.split('/').slice(-2)[0]
 </script>
 
 
@@ -17,7 +20,7 @@ const { data: start_page } = await useAsyncData("start_page", () => {
   <div class="flex grow justify-center items-center">
     <div data-testid="contentwrapper" class="flex flex-col justify-center items-center text-center space-y-24 py-12">
       <h1 class="text-9xl font-bold dark:drop-shadow-glow-dark-lg drop-shadow-glow-bright-lg">{{ currentcourse }}</h1>
-      <UButton variant="outline" size="xl" class="px-14 py-4 text-3xl" :to="start_page?.path">Start</UButton>
+      <UButton variant="outline" size="xl" class="px-14 py-4 text-3xl" :to="start_route">Start</UButton>
     </div>
   </div>
 </template>
