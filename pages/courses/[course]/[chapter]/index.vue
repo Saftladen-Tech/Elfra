53cdb25fe2bca3d2afd7a8842be315a47900304c
Flo <93609174+Xeferis@users.noreply.github.com>
2025-05-05 21:12:30 +0200

    fixed description not beeing displayed

--- pages/courses/[course]/[chapter]/index.vue
+++ pages/courses/[course]/[chapter]/index.vue
@@ -11,6 +11,13 @@ const { data: start_page } = await useAsyncData("start_page", () => {
     .first()
 })
 
+const { data: chapters } = await useAsyncData("chapters", () => {
+  return queryCollection('folders')
+    .where('stem', 'LIKE', "%/"+ currentcourse.value + "/" + currentchapter + "/%")
+    .andWhere(query => query.where('type', '=', "chapter"))
+    .first()
+})
+
 </script>
 
 
@@ -18,7 +25,7 @@ const { data: start_page } = await useAsyncData("start_page", () => {
   <div class="flex justify-center items-center grow">
       <div data-testid="contentwrapper" class="flex flex-col justify-center items-center max-w-[800px] space-y-8">
         <h1 class="text-8xl font-semibold dark:drop-shadow-glow-lg drop-shadow-glow-dark-lg">{{ route.params.chapter }}</h1>
-        <p class="text-4xl text-center bg-transparent">Hier kommt eine kurze beschreibung der Topic hin. Kann alles mögliche sein!</p>
+        <p class="text-4xl text-center bg-transparent">{{ chapters?.description }}</p>
         <UButton data-testid="startbtn" variant="ghost" size="xl" icon="i-heroicons-arrow-down" :to="start_page?.path"></UButton>
       </div>
   </div>
