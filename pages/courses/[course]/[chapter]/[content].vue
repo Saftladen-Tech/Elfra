da3739cb9b50bf8d2bb4292d035191fc22c362c1
Florian <93609174+Xeferis@users.noreply.github.com>
2025-05-05 11:19:12 +0000

    Improved Structure

--- pages/courses/[course]/[chapter]/[content].vue
+++ pages/courses/[course]/[chapter]/[content].vue
@@ -18,9 +18,9 @@ const { data } = await useAsyncData('surround', () => {
 
 <template>
   <div class="flex flex-col min-h-screen xl:px-28 my-20 grow">
-    <h1 id="md-course" class="text-5xl font-bold text-prmry-500">{{ page?.title }}</h1>
+    <h1 id="md-course" class="text-5xl font-bold text-prmry-500">{{ currentcourse }} - {{ route.params.chapter }}</h1>
     <div data-testid="content" class="bg-white p-5 rounded-lg min-h-full my-4 text-black border light:border-black dark:border-prmry-500/50">
-      <h2 id="md-topic" class="text-4xl font-semibold mb-5">{{ page?.description }}</h2>
+      <h2 id="md-topic" class="text-4xl font-semibold mb-5">{{ page?.title }}</h2>
       <ContentRenderer class="text-xl font-regular" :value="page?.body" />
     </div>
     <div id="buttons" class="flex justify-between my-3">
