7488a8d2eb85cd24b7cfd0df9a69d0cada50f564
Flo <93609174+Xeferis@users.noreply.github.com>
2025-08-28 20:12:24 +0200

    Entrypoint not working, but generated

---
+++ utils/shell/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Verzeichnisse definieren
+configs="./.default/config"
+courses="./.default/courses"
+config_mount="./customconfig"
+courses_mount="./content/courses"
+
+# Funktion zum Kopieren, wenn Ziel leer ist
+copy_if_empty() {
+    SRC="$1"
+    DEST="$2"
+    if [ -d "$DEST" ] && [ -z "$(ls -A "$DEST")" ]; then
+        cp -a "$SRC/" "$DEST/"
+        echo "Daten von $SRC nach $DEST kopiert."
+    else
+        echo "$DEST ist nicht leer, keine Daten kopiert."
+    fi
+}
+
+copy_if_empty "$configs" "$config_mount"
+copy_if_empty "$courses" "$courses_mount"
