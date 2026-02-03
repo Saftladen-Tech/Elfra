1e8b71b582416e6645a224a8c9400fe32ffa31b3
Florian <93609174+Xeferis@users.noreply.github.com>
2026-02-03 11:51:00 +0000

    Fixed URLS

--- test/chapter-content-test.spec.ts
+++ test/chapter-content-test.spec.ts
@@ -7,25 +7,25 @@ describe("Chapter-Content test", async () => {
   });
 
   it("Has Chapter Heading", async () => {
-    const page = await createPage("/chapter_content");
+    const page = await createPage("/courses/elfra-getting-started/introduction/start");
     expect(await page.getByRole("heading", { level: 1 }).count()).toBe(1);
   });
 
   it("Has Topic Heading", async () => {
-    const page = await createPage("/chapter_content");
+    const page = await createPage("/courses/elfra-getting-started/introduction/start");
     const content = page.getByTestId("content");
     expect(await content.getByRole("heading", { level: 2 }).count()).toBe(1);
   });
 
   it("Has Nav Buttons next", async () => {
-    const page = await createPage("/chapter_content");
+    const page = await createPage("/courses/elfra-getting-started/introduction/start");
     expect(
       await page.getByRole("link", { name: "Weiter", exact: true }).count()
     ).toBe(1);
   });
 
   it("Has Nav Buttons prev", async () => {
-    const page = await createPage("/chapter_content");
+    const page = await createPage("/courses/elfra-getting-started/introduction/installation-1");
     expect(
       await page.getByRole("link", { name: "Zurück", exact: true }).count()
     ).toBe(1);
